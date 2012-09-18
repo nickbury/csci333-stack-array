37ca9743842e6434a18580fbde4f422bb0e421c1
Nick Bury <nick@debian.(none)>
2012-09-17 22:01:33 -0400

    modified to double stack size when needed

--- src/Stack.cpp
+++ src/Stack.cpp
@@ -4,6 +4,7 @@
 Stack::Stack(int initialSize) {
   theStack = new int[initialSize];
   top = 0;
+  capacity = initialSize;
 }
 
 Stack::~Stack() {
@@ -11,13 +12,26 @@ Stack::~Stack() {
 }
 
 void Stack::push(int value) {
-  //if theStack is full
+  if(top==(capacity-1)){    
   // create new stack twice as big
+    int c = capacity*2;
+    int* s = new int[c];
   // copy all elements to new stack
+      for(int i=0; i<capacity; ++i) {
+        s[i]=theStack[i];
+      }
+  // push value onto new stack
+    s[top] = value;
+    top++;
   // delete old stack
+    delete theStack;
   // point old stack pointer to new stack
+    capacity = c;
+    theStack = s;
+  } else {
   theStack[top] = value;
   top++;
+  }
 }
 
 int Stack::pop() {
