67ea0c20f7c9f530637a1afc01efee73843e422f
Nick Bury <nick@debian.(none)>
2012-09-17 22:02:37 -0400

    added stack resizing test

--- test/Stack.cpp
+++ test/Stack.cpp
@@ -9,4 +9,15 @@ TEST(StackTest, Push) {
   delete s;
 }
 
+TEST(StackTest, Resize) {
+  Stack* s = new Stack(2);
+  s->push(1);
+  s->push(2);
+  s->push(3);
 
+  EXPECT_EQ(3, s->peek());
+  s->push(4);
+  s->push(5);
+  EXPECT_EQ(5, s->peek());
+  delete s;
+}
